d88db7289c43c400d50839f8170713b9af9333c0
Killitar <78868798+Killitar@users.noreply.github.com>
2022-12-10 21:21:07 +0300

    Update lab_7.py

--- lab_7.py
+++ lab_7.py
@@ -1,23 +1,16 @@
-\
-import math
-# fx = open("laba_1","w")
-fh = open("laba_1","r")
-x = int(fh.read())
-y = 1
+from math import *
 
-if x >= -9 and x < -5:
-    print("x = {0}, y = {1}".format(x, -math.sqrt(4 - (x + 7) ** 2) + 2))
-elif x >= -5 and x < -4:
-    print("x = {0}, y = {1}".format(x, 1))
-elif x >= -4 and x < 0:
-    print("x={0}, y = {1}".format(x, y / 2))
-elif x >= 0 and x < math.pi:
-    print("x={0}, y ={1}".format(x, math.sin(x)))
-elif x >= math.pi and x <= 5:
-    print("x={0}, y = {1}".format(x, x))
-else:
-    print("error")
-# fx.close()
+fa = open("laba_1_a.txt","wt")
+fh = open("laba_1.txt","rt")
+
+a=float(fh.read()) 
+z1 =(sin(2*a)+sin(5*a) - sin(3*a)) / (cos(a) + 1-2*(1-cos(2*a))) 
+z2 = 2*sin(a) 
+
+fa.write(str(z1)) 
+fa.write('\n')
+fa.write(str(z2))
+fa.close()
 fh.close()
 
 #2
@@ -25,7 +18,8 @@ import math
 
 print("Введите координаты x,y,r")
 
-fh = open("laba_2","r")
+fh = open("laba_2","rt")
+fa = open("laba_2_a","wt")
 r = float(fh.readline())
 x = float(fh.readline())
 y = float(fh.readline())
@@ -39,7 +33,43 @@ if(
     and x>=math.pow((x+r),2) + pow((y-r),2) +r**2
     
 ):
-    print("Попадает")
+    fa.write("Попадает")
 else:
-    print("Не попадает")
+    fa.write("Не попадает")
 fh.close()
+fh.close()
+
+#3
+from math import pow, factorial
+ 
+print("Введите x_beg,x_end,d_x,eps")
+ 
+fa = open("laba_2_a.txt","wt")
+fh = open("laba_2.txt","rt")
+
+xb = float(fh.readline())
+xe = float(fh.readline())
+dx = float(fh.readline())
+eps = float(fh.readline())
+ 
+xt = xb
+ 
+fa.write("+---------+---------+---------+ \n")
+fa.write("I    X    I    Y    I    N    I \n")
+fa.write("+---------+---------+---------+ \n")
+ 
+while xt <= xe:
+    an = xt
+    n = 0
+    y = an
+    while True:
+        k = pow(xt, n) / factorial(n)
+        an = an * k
+        y += an
+        n += 1
+        if abs(an) < eps:
+            break
+    fa.write("I{0:7.2f}  I {1:7.3f} I{2:4f} I".format(xt, y, n))
+    xt += dx
+fa.write("\n+---------+---------+---------+")
+
